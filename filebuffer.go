26b7017225402865abaac3330e93aa169bb23049
Matt Aimonetti <mattaimonetti@gmail.com>
2016-04-09 23:41:19 -0700

    add io.Closer to the list of implemented interfaces

--- filebuffer.go
+++ filebuffer.go
@@ -1,7 +1,6 @@
 // filebuffer is a package implementing a few file like interfaces
 // backed by a byte buffer.
-// Reader ReaderAt Writer Seeker
-// TODO: Closer
+// Reader ReaderAt Writer Seeker Closer
 package filebuffer
 
 import (
@@ -17,7 +16,8 @@ type Buffer struct {
 	// Buff is the backing buffer
 	Buff *bytes.Buffer
 	// Index indicates where in the buffer we are at
-	Index int64
+	Index    int64
+	isClosed bool
 }
 
 // New returns a new populated Buffer
@@ -25,12 +25,12 @@ func New(b []byte) *Buffer {
 	return &Buffer{Buff: bytes.NewBuffer(b)}
 }
 
-// Bytes returns the bytes
-func (fbuff *Buffer) Bytes() []byte {
-	if fbuff.Index >= int64(fbuff.Buff.Len()) {
+// Bytes returns the bytes available until the end of the buffer.
+func (f *Buffer) Bytes() []byte {
+	if f.isClosed || f.Index >= int64(f.Buff.Len()) {
 		return []byte{}
 	}
-	return fbuff.Buff.Bytes()[fbuff.Index:]
+	return f.Buff.Bytes()[f.Index:]
 }
 
 // Read reads up to len(p) bytes into p. It returns the number of bytes read (0 <= n <= len(p))
@@ -43,15 +43,18 @@ func (fbuff *Buffer) Bytes() []byte {
 // return the error (and n == 0) from a subsequent call. An instance of this general case is
 // that a Reader returning a non-zero number of bytes at the end of the input stream may return
 // either err == EOF or err == nil. The next Read should return 0, EOF.
-func (fbuff *Buffer) Read(b []byte) (n int, err error) {
+func (f *Buffer) Read(b []byte) (n int, err error) {
+	if f.isClosed {
+		return 0, io.EOF
+	}
 	if len(b) == 0 {
 		return 0, nil
 	}
-	if fbuff.Index >= int64(fbuff.Buff.Len()) {
+	if f.Index >= int64(f.Buff.Len()) {
 		return 0, io.EOF
 	}
-	n, err = bytes.NewBuffer(fbuff.Buff.Bytes()[fbuff.Index:]).Read(b)
-	fbuff.Index += int64(n)
+	n, err = bytes.NewBuffer(f.Buff.Bytes()[f.Index:]).Read(b)
+	f.Index += int64(n)
 
 	return n, err
 }
@@ -72,17 +75,20 @@ func (fbuff *Buffer) Read(b []byte) (n int, err error) {
 // If ReadAt is reading from an input source with a seek offset,
 // ReadAt should not affect nor be affected by the underlying seek offset.
 // Clients of ReadAt can execute parallel ReadAt calls on the same input source.
-func (fbuff *Buffer) ReadAt(p []byte, off int64) (n int, err error) {
+func (f *Buffer) ReadAt(p []byte, off int64) (n int, err error) {
+	if f.isClosed {
+		return 0, io.EOF
+	}
 	if off < 0 {
 		return 0, errors.New("filebuffer.ReadAt: negative offset")
 	}
 	reqLen := len(p)
-	buffLen := int64(fbuff.Buff.Len())
+	buffLen := int64(f.Buff.Len())
 	if off >= buffLen {
 		return 0, io.EOF
 	}
 
-	n = copy(p, fbuff.Buff.Bytes()[off:])
+	n = copy(p, f.Buff.Bytes()[off:])
 	if n < reqLen {
 		err = io.EOF
 	}
@@ -90,6 +96,9 @@ func (fbuff *Buffer) ReadAt(p []byte, off int64) (n int, err error) {
 }
 
 func (f *Buffer) Write(p []byte) (int, error) {
+	if f.isClosed {
+		return 0, io.EOF
+	}
 	if f.Index < 0 {
 		return 0, io.EOF
 	}
@@ -105,22 +114,31 @@ func (f *Buffer) Write(p []byte) (int, error) {
 	return n, err
 }
 
-func (fbuff *Buffer) Seek(offset int64, whence int) (idx int64, err error) {
+func (f *Buffer) Seek(offset int64, whence int) (idx int64, err error) {
+	if f.isClosed {
+		return 0, io.EOF
+	}
 
 	var abs int64
 	switch whence {
 	case 0:
 		abs = offset
 	case 1:
-		abs = int64(fbuff.Index) + offset
+		abs = int64(f.Index) + offset
 	case 2:
-		abs = int64(fbuff.Buff.Len()) + offset
+		abs = int64(f.Buff.Len()) + offset
 	default:
 		return 0, errors.New("filebuffer.Seek: invalid whence")
 	}
 	if abs < 0 {
 		return 0, errors.New("filebuffer.Seek: negative position")
 	}
-	fbuff.Index = abs
+	f.Index = abs
 	return abs, nil
+}
+
+// Close closes the File, rendering it unusable for I/O. It returns an error, if any.
+func (f *Buffer) Close() error {
+	f.isClosed = true
+	return nil
 }
